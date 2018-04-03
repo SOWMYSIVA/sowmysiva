384599fb5ba6e4f84868cbdfc2072a185b67ec9c
SOWMYSIVA <37694343+SOWMYSIVA@users.noreply.github.com>
2018-04-03 15:15:32 +0530

    Create vow.c

---
+++ vow.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char l;
+printf("enter the letter");
+scanf("%c",&l);
+if(l==a||l==e||l==o||l==i||l==u)
+{
+printf("vowel");
+}
+else
+{
+printf("not vowel");
+}}
