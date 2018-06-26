c6329b9cde70587e9ac6cb676e5b00ddb23c05cf
SOWMYSIVA <37694343+SOWMYSIVA@users.noreply.github.com>
2018-06-26 13:10:47 +0000

    allarray.java

---
+++ allarray.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class allarray
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int n1=sc.nextnt();
+int m[][]=new int[n][n1];
+for(i=0;i<n;i++)
+{
+for(int j=0;j<n1;j++)
+{
+m[i][j]=sc.nextInt();
+}
+}
+
+}
+}
