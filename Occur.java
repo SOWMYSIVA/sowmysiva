95de39f776ecbcd129fa434f6308d54f6b3d9619
SOWMYSIVA <37694343+SOWMYSIVA@users.noreply.github.com>
2018-06-26 05:26:41 +0000

    Create Occur.java

---
+++ Occur.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.uitl.*;
+class Occur
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int a[]=new int[n];
+int count=0;
+for(int i=0;i<n;i++)
+{
+for(int j=0;j<n;j++)
+{
+
+}
+}
+
+}
+}
