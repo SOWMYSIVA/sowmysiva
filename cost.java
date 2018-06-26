7cd88e2d13cd0afc931872e645068f50f86f945f
SOWMYSIVA <37694343+SOWMYSIVA@users.noreply.github.com>
2018-06-26 11:44:24 +0000

    Update cost.java

--- cost.java
+++ cost.java
@@ -5,12 +5,12 @@ class cost
 public static void main(String arg[])
 {
 Scanner sc=new Scanner(System.in);
-String x=sc.next();
-String y=sc.next();
+String x=sc.next().toUpperCase();
+String y=sc.next().toUpperCase();
 int count=0;
 for(int i=0;i<x.length();i++)
 {
-if(String.valueOf(x.charAt(i)).equalsIgnoreCase(String.valueOf(y.charAt(i))))
+if(String.valueOf(x.charAt(i)).equals(String.valueOf(y.charAt(i))))
 count++;
 }
 int v=y.length()-count;
