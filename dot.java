54ad16de47a6f5dc8d2e6842d2514e9cf7967f76
SOWMYSIVA <37694343+SOWMYSIVA@users.noreply.github.com>
2018-06-27 09:27:11 +0000

    dot.java

---
+++ dot.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class dot
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.next();
+System.out.print(s+".");
+}
+}
