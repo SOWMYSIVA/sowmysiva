d4e8ffd9546373d757decf4156784b0574fff7dd
SOWMYSIVA <37694343+SOWMYSIVA@users.noreply.github.com>
2018-07-12 12:29:34 +0530

    Partition.java

---
+++ Partition.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class Partition
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int a=sc.nextInt();
+int b=sc.nextI
+}
+}
