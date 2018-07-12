6620a18d57ef24dbe6ac3a1e2f446bb2a6c7ab39
SOWMYSIVA <37694343+SOWMYSIVA@users.noreply.github.com>
2018-07-12 12:03:51 +0530

    ConMax.java

---
+++ ConMax.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class ConMax
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+ a[i]=sc.nextInt();
+}
+for(int i=0;i<n-1;i++)
+{
+    if(a[i]>a[i+1])
+        System.out.print(a[i]);
+        else
+        System.out.print(a[i+1]);
+}
+}
+}
