f01b2c4ffe75cebda893f3130879370c11e1de9a
SOWMYSIVA <37694343+SOWMYSIVA@users.noreply.github.com>
2018-07-12 12:12:58 +0530

    ConMax.java

--- ConMax.java
+++ ConMax.java
@@ -14,9 +14,9 @@ for(int i=0;i<n;i++)
 for(int i=0;i<n-1;i++)
 {
     if(a[i]>a[i+1])
-        System.out.print(a[i]);
+        System.out.print(a[i]+" ");
         else
-        System.out.print(a[i+1]);
+        System.out.print(a[i+1]+" ");
 }
 }
 }
